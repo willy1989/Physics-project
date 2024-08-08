c4ed44001fa08aefb2be4043a95e9c596e47c311
willy1989 <willy.winelover@gmail.com>
2024-08-08 09:49:10 +0200

    Added more playmode tests

--- Assets/Tests/PlayMode/FirstKinematicEquationTests.cs
+++ Assets/Tests/PlayMode/FirstKinematicEquationTests.cs
@@ -8,18 +8,28 @@ public class FirstKinematicEquationTests
 {
     public class ConstantForce
     {
-        [UnityTest]
-        public IEnumerator Move_Object_Of_1kg_With_Force_1_To_00_00_1_In_2_Seconds()
-        {
-            KinematicEquations kinematicEquationsComponent = new GameObject().AddComponent<KinematicEquations>();
+        private KinematicEquations kinematicEquationsComponent;
 
-            Constant_ForceType constantForceComponent = new GameObject().AddComponent<Constant_ForceType>();
+        private Constant_ForceType constantForceComponent;
+
+        private GameObject physicsObject;
+
+        [SetUp]
+        public void SetUpTest()
+        {
+            kinematicEquationsComponent = new GameObject().AddComponent<KinematicEquations>();
+
+            constantForceComponent = new GameObject().AddComponent<Constant_ForceType>();
+
+            physicsObject = new GameObject();
+        }
+
+        [UnityTest]
+        public IEnumerator Move_Object_1kg_With_Force_1_In_2_Seconds_FrameRate_60_To_00_00_02()
+        {
+            // Arrange
 
             constantForceComponent.SetUp(_force: 1f, _dimension: DirectionDimension.ZPOSITIVE);
-
-
-            // Physics object
-            GameObject physicsObject = new GameObject();
 
             OneDimensionForceApplier oneDimensionForceApplier = physicsObject.AddComponent<OneDimensionForceApplier>();
 
@@ -28,18 +38,117 @@ public class FirstKinematicEquationTests
             float elapsedTime = 0;
             float duration = 2f;
 
-
             while(elapsedTime < duration)
             {
                 elapsedTime += Time.deltaTime;
                 yield return null;
             }
 
-            Debug.Log(physicsObject.transform.position);
+            Application.targetFrameRate = 60;
 
-            Assert.AreEqual(physicsObject.transform.position, new Vector3(0f, 0f, 1f));
+
+            // Assert
+
+            Vector3 expectedPosition = new Vector3(0f, 0f, 2f);
+
+            float deltaBetweenExpectedAndActualPosition = (expectedPosition - physicsObject.transform.position).magnitude;
+
+            Assert.Less(deltaBetweenExpectedAndActualPosition, 0.1f);
+        }
+
+        [UnityTest]
+        public IEnumerator Move_Object_1kg_With_Force_1_In_2_Seconds_FrameRate_30_To_00_00_02()
+        {
+            // Arrange
+
+            constantForceComponent.SetUp(_force: 1f, _dimension: DirectionDimension.ZPOSITIVE);
+
+            OneDimensionForceApplier oneDimensionForceApplier = physicsObject.AddComponent<OneDimensionForceApplier>();
+
+            oneDimensionForceApplier.SetUp(_kinematicEquations: kinematicEquationsComponent, _forceTypes: new ForceType[] { constantForceComponent }, _mass: 1);
+
+            float elapsedTime = 0;
+            float duration = 2f;
+
+            while (elapsedTime < duration)
+            {
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            Application.targetFrameRate = 30;
+
+
+            // Assert
+
+            Vector3 expectedPosition = new Vector3(0f, 0f, 2f);
+
+            float deltaBetweenExpectedAndActualPosition = (expectedPosition - physicsObject.transform.position).magnitude;
+
+            Assert.Less(deltaBetweenExpectedAndActualPosition, 0.1f);
+        }
+
+        [UnityTest]
+        public IEnumerator Move_Object_2kg_With_Force_1_In_10_Seconds_FrameRate_60_To_00_00_25()
+        {
+            // Arrange
+
+            constantForceComponent.SetUp(_force: 1f, _dimension: DirectionDimension.ZPOSITIVE);
+
+            OneDimensionForceApplier oneDimensionForceApplier = physicsObject.AddComponent<OneDimensionForceApplier>();
+
+            oneDimensionForceApplier.SetUp(_kinematicEquations: kinematicEquationsComponent, _forceTypes: new ForceType[] { constantForceComponent }, _mass: 2);
+
+            float elapsedTime = 0;
+            float duration = 10f;
+
+            while (elapsedTime < duration)
+            {
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            Application.targetFrameRate = 60;
+
+
+            // Assert
+
+            Vector3 expectedPosition = new Vector3(0f, 0f, 25f);
+
+            float deltaBetweenExpectedAndActualPosition = (expectedPosition - physicsObject.transform.position).magnitude;
+
+            Assert.Less(deltaBetweenExpectedAndActualPosition, 0.1f);
+        }
+
+        [UnityTest]
+        public IEnumerator Move_Object_5kg_With_Force_Minus3_In_3_Seconds_FrameRate_20_To_00_Minus02Point07_00()
+        {
+            // Arrange
+
+            constantForceComponent.SetUp(_force: 3f, _dimension: DirectionDimension.YNEGATIVE);
+
+            OneDimensionForceApplier oneDimensionForceApplier = physicsObject.AddComponent<OneDimensionForceApplier>();
+
+            oneDimensionForceApplier.SetUp(_kinematicEquations: kinematicEquationsComponent, _forceTypes: new ForceType[] { constantForceComponent }, _mass: 5);
+
+            float elapsedTime = 0;
+            float duration = 3f;
+
+            while (elapsedTime < duration)
+            {
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            Application.targetFrameRate = 60;
+
+            // Assert
+
+            Vector3 expectedPosition = new Vector3(0f, -2.7f, 0f);
+
+            float deltaBetweenExpectedAndActualPosition = (expectedPosition - physicsObject.transform.position).magnitude;
+
+            Assert.Less(deltaBetweenExpectedAndActualPosition, 0.1f);
         }
     }
-
-    
 }
